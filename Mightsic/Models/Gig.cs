396ae8370d55bc830c2d6f517a7a12edf2ad59a2
Jean Jordan <jeanpierre@multiplica.com>
2018-01-29 15:47:45 -0500

    Initial commit

---
+++ Mightsic/Models/Gig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mightsic.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
