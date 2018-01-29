67c026c32a772f2f88edd141e017038371785a08
Jean Jordan <jeanpierre@multiplica.com>
2018-01-29 14:45:55 -0500

    Agregar archivos de proyecto.

---
+++ Mightsic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mightsic.MvcApplication" Language="C#" %>
---
+++ Mightsic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mightsic.Startup))]
+namespace Mightsic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
