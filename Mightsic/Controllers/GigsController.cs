2c95a6c0347536c8ddcd50d57d79b03bfb7645db
Jean Jordan <jeanpierre@multiplica.com>
2018-01-30 11:12:46 -0500

    Back

---
+++ Mightsic/Controllers/GigsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Mightsic.Models;
+using Mightsic.ViewModels;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+namespace Mightsic.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Mightsic/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Mightsic.Models;
+using System.Collections.Generic;
+
+namespace Mightsic.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
